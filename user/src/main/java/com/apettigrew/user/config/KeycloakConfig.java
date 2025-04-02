0489fab09e64b3e9b8355fed9635a153dc3e849a
Andrew Pettigrew <devdrew10@gmail.com>
2025-04-02 15:35:18 -0400

    feat: small code fix

--- user/src/main/java/com/apettigrew/user/config/KeycloakConfig.java
+++ user/src/main/java/com/apettigrew/user/config/KeycloakConfig.java
@@ -1,17 +1,23 @@
 package com.apettigrew.user.config;
 
+import jakarta.ws.rs.client.ClientBuilder;
 import lombok.RequiredArgsConstructor;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 @RequiredArgsConstructor
 public class KeycloakConfig {
-
+    private static final Logger logger = LoggerFactory.getLogger(KeycloakConfig.class);
     private final KeycloakConfigProperties keycloakConfigProperties;
 
     @Bean
@@ -28,6 +34,10 @@ public class KeycloakConfig {
                 .grantType(OAuth2Constants.PASSWORD)
                 .username(keycloakConfigProperties.getUserName())
                 .password(keycloakConfigProperties.getPassword())
+                .resteasyClient(((ResteasyClientBuilder) ClientBuilder.newBuilder())
+                        .connectionPoolSize(3)
+                        .connectionTTL(10, TimeUnit.SECONDS)
+                        .build())
                 .build();
     }
 }
